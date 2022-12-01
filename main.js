ed82845d9b064a7482f1d66eea163e204e3c91f0
MATHEUS MARQUES BANDEIRA <632220119@fspoa.br>
2022-12-01 11:36:29 -0300

    pega dados do usuario na autenticação

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ function handleCredentialResponse(response) {
    const data = jwt_decode(response.credential)
 
    if(data != null){ 
-      window.location.href = "/index.html";
+
+      var conteudo = "<h1> ID: " + data.sub+"</h1>";
+      conteudo +="Image URL: <img src='"+data.picture+"'>"+"<br>";
+      conteudo +='Full Name: ' +data.name+"<br>";
+      conteudo +='Given Name: ' +data.given_name+"<br>";
+      conteudo +='Family Name: ' + data.family_name+"<br>";
+      conteudo +="Email: " + data.email+"<br>";
+     document.getElementById("user").innerHTML = conteudo;
+     window.location.href = "/index.html"
    }
    else{
       document.getElementById("user").innerHTML = "<h1>Favor Realizar o login</h1>";
